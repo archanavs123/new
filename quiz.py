71713c2bdaa385f5d098479a18c535a7b69c9178
archana <archanavs189@gmail.com>
2023-07-18 09:00:34 +0530

    . file added

---
+++ quiz.py
@@ -0,0 +1,12 @@
+print("which is the largest river in kerala")
+a="nila"
+b=["chandragiri","kabani","bavani"]
+print("a.nila" )
+print("b.chandragiri") 
+print("c.kabani")
+print("d.bavani")
+e=input("choose your answer:")
+if(e in b):
+     print("wrong answer") 
+else:
+      print("correct answer")
