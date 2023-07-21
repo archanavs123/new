1cd543f013a04be59690c0b5d22866de686e31fe
archana <archanavs189@gmail.com>
2023-07-21 18:13:08 +0530

    adding

--- conditional.py
+++ conditional.py
@@ -24,12 +24,13 @@ else:
 
 #elif(largest among 3)
 
+
 x=int(input("Enter First Number:"))
 y=int(input("Enter Second Number:"))
 z=int(input("Enter Third Number:"))
-if x>y>z:
+if x>y and x>z:
     print(x,"is the largest number") 
-elif y>x>z:
+elif y>x and y>z:
      print(y,"is the largest number")
 else :
      print(z,"is the largest number") 
