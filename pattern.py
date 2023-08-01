497d277231c445058aa5a17fc7ca9c2368c64d45
archana <archanavs189@gmail.com>
2023-08-01 09:00:27 +0530

    edited

--- pattern.py
+++ pattern.py
@@ -5,18 +5,23 @@
 # for i in range(0,limit):
 #     print("*"*4)
 
-#pattern triangle
+# pattern triangle
 # ----------------
 # limit=int(input("Enter the row:"))
 # for i in range(1,limit):
-#    #for e in range(0,i+1):
 #      print("*"*i)
 
 
-
-
-
+#pattern invert triangle
+#-----------------------
 limit=int(input("Enter the row:"))
-for i in range(1,limit):
-   #for e in range(0,i+1):
-     print("*"*i)
+for i in range(0,limit+1):
+     s=limit-i
+     print(" "*s,"*"*i)
+
+#pattern
+#-------
+# limit=int(input("Enter the row:"))
+# for i in range(0,limit-1):
+#      print("*"*i," "*s)
+#      s=i-limit
