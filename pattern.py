5b728e79321c66a7856ae1dfcb580c8d1d55293b
archana <archanavs189@gmail.com>
2023-08-09 19:47:34 +0530

    pattern

--- pattern.py
+++ pattern.py
@@ -14,10 +14,10 @@
 
 #pattern invert triangle
 #-----------------------
-limit=int(input("Enter the row:"))
-for i in range(0,limit+1):
-     s=limit-i
-     print(" "*s,"*"*i)
+# limit=int(input("Enter the row:"))
+# for i in range(0,limit+1):
+#      s=limit-i
+#      print(" "*s,"*"*i)
 
 #pattern
 #-------
