58d0eec5ac6d474b23f59576fe8a2712fa453a71
archana <archanavs189@gmail.com>
2023-08-10 09:22:52 +0530

    initial commit

--- nmbrptrn6.py
+++ nmbrptrn6.py
@@ -8,10 +8,48 @@
 
 
 
-limit=int(input("Enter the raw:"))
-num=1
-for i in range(limit,1):
-     for j in range(i+1,limit):
-          print(num,end='')
-          num+=1
-     print()
+# limit=int(input("Enter the raw:"))
+# num=1
+# for i in range(limit,0,-1):
+#      for j in range(i+1,1):
+#           print(num,end='')
+#           num+=1
+#      print()   
+
+# ASCII VALUES:
+# A = 65
+# Z = 90 
+
+# chr()
+
+# for i in range(65,91):
+#      print(chr(i)) 
+
+# A 
+# A B 
+# A B C 
+# A B C D 
+
+
+# A A A A 
+# B B B B 
+# C C C C 
+# D D D D 
+
+
+# A 
+# B B 
+# C C C 
+# D D D D 
+
+# while 
+
+# initialization 
+# while + condition 
+# statements 
+# updation 
+
+# i = 1 
+# while i <=10:
+#     print(i)
+#     i+=1 
